087f8e4e40f8c1552b005ef71196c0f86c0529eb
*vk-amogh* <*amogh.vk.2005@gmail.com*>
2025-12-12 21:31:10 +0530

    Remove driftinn_app folder

--- driftinn_app/android/app/src/main/kotlin/com/driftinn/driftinn/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.driftinn.driftinn
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- driftinn_app/lib/providers/app_providers.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// Placeholder for future providers
-// final authUserProvider = StreamProvider<User?>((ref) => ...);
